eee37b6f21c046fb63c381cc2f8b3d5ea7138d2d
imhilla <hillaryodhiambo282@gmail.com>
2020-08-14 13:27:50 +0300

    override devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+           :sessions => "users/sessions",
+           :registrations => "users/registrations" }
 end
