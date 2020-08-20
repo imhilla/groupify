f3b38dced169433933cbf1055b0852602f8a9ade
imhilla <hillaryodhiambo282@gmail.com>
2020-08-20 17:45:58 +0300

    add tests for externals controller

---
+++ spec/controllers/externals_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'ancients', type: :system do
+  describe 'index page' do
+    it 'shows the right content in the externals page' do
+      do_signup('hillary', 'hillary', 'hillaryodhiambo@gmail.com', '123456')
+      click_button(class: 'openbtn')
+      click_link('side-link2')
+      click_link('+')
+      create_article
+      click_button(class: 'openbtn')
+      click_link('side-link4')
+      expect(page).to have_content 'Capstone projects are projects to test your mastery of a particular section'
+    end
+  end
+end
