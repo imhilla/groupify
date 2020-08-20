4a18fc134baa6174c0b35ea6ecbbcfe2fcec820a
imhilla <hillaryodhiambo282@gmail.com>
2020-08-20 21:18:02 +0300

    add test for likes model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'associations' do
+    it { should belong_to(:article) } 
+    it { should belong_to(:user) }
+  end
+
+  before(:all) do
+    User.create(name: 'hasan', username: 'hasan', email: 'ozovalihasan@gmail.com', password: '123456')
+    Article.create(title: 'my name', body: 'my name is hillary', author_id: 1)
+    Like.create(user_id: 1, article_id: 1)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(User.last.likes.count).to eql 1
+  end
+end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe User, type: :model do
       allowing('image/png', 'image/gif').
       rejecting('text/plain', 'text/xml')}
   end
+
+  before(:all) do
+    User.create(name: 'hasan', username: 'hasan', email: 'ozovalihasan@gmail.com', password: '123456')
+  end
+  it 'creates a user' do
+    expect(User.all.count).to be_eql 1
+  end
 end
