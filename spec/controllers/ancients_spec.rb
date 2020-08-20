69e86f4678001d79e068052b87e5a416ffcabe72
imhilla <hillaryodhiambo282@gmail.com>
2020-08-20 17:07:38 +0300

    add test for create article

--- spec/controllers/ancients_spec.rb
+++ spec/controllers/ancients_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'ancients', type: :system do
-  before(:all) do
-    User.create(name: 'hillary', username: 'imhilla', email: 'hillaryodhiambo282@gmail.com', password: '123456',  password_confirmation: '123456')
-  end
   describe 'index page' do
     it 'shows the right content' do
-      do_signup('hillary', 'imhilla', 'hillaryodhiambo282@gmail.com', '123456')
-      do_login('imhilla', '123456')
-      visit articles_path
-      sleep(5)
-
-      expect(page).to have_content('no more posts')
+      do_signup('hillary', 'hillary', 'hillaryodhiambo@gmail.com', '123456')
+      click_button(class: 'openbtn')
+      click_link('side-link2')
+      click_link('+')
+      expect(page).to have_content 'Body'
     end
   end
 end
--- spec/support/integration_test_helpers.rb
+++ spec/support/integration_test_helpers.rb
@@ -4,8 +4,8 @@ module IntegrationTestHelpers
     click_button('SIGN UP')
     attach_file("user[image]", "#{Rails.root}/spec/files/attachment.jpeg", visible: false)
     find("input[placeholder='Name']").set 'hillary'
-    find("input[placeholder='Username']").set 'imhilla'
-    find("input[placeholder='Email']").set 'hillaryodhiambo282@gmail.com'
+    find("input[placeholder='Username']").set 'hillary'
+    find("input[placeholder='Email']").set 'hillaryodhiambo@gmail.com'
     find("input[placeholder='Password']").set '123456'
     find("input[placeholder='Password confirmation']").set '123456'
     click_button('sign_up')
@@ -14,8 +14,15 @@ module IntegrationTestHelpers
   def do_login(username, password)
     visit root_path
     click_button('LOG IN')
-    find("input[placeholder='Username']").set 'imhilla'
+    find("input[placeholder='Username']").set 'hillary'
     find("input[placeholder='Password']").set '123456'
     click_button('sign_in')
+  end
+
+  def create_article
+    fill_in('Title', :with => 'What is a capstone project')
+    fill_in('Title', :with => 'What is a capstone project?')
+    fill_in('Body', :with => 'Capstone projects are projects to test your mastery of a particular section')
+    click_button('Create article')
   end
 end
