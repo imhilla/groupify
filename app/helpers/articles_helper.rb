d9a8992b4fb0f2d966b0b8f57715f048935fd70b
imhilla <hillaryodhiambo282@gmail.com>
2020-08-22 20:39:39 +0300

    refactor all if statements to views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,9 +1,15 @@
 module ArticlesHelper
   def article_hash_tag(article)
     if article.group_id.present?
-      link_to Group.find(article.group_id).name, group_path(article.group_id) 
+      link_to  Group.find(article.group_id).name, group_path(article.group_id) 
     else
-      link_to '#Groupify'
+      link_to 'Groupify'
+    end
+  end
+
+  def article_image(article)
+     if article.group_id.present?
+       image_tag Group.find(article.group_id).icon.url(:medium)
     end
   end
 
