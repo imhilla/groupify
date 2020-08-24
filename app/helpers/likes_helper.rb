d3469cf76a245c5e71ef05326eaa9f810cbf2763
imhilla <hillaryodhiambo282@gmail.com>
2020-08-24 15:53:55 +0300

    add ancients like button and count

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -6,6 +6,6 @@ module LikesHelper
 
   def like_btn(article)
     like = Like.find_by(article: article, user: current_user)
-    button_to('Like!', article_likes_path(article_id: article.id), method: :post)
+    button_to('', article_likes_path(article_id: article.id), method: :post, :class => 'like-button-dynamic')
   end
 end
