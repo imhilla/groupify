2808542a61969013d642cfff735b444e16cea827
imhilla <hillaryodhiambo282@gmail.com>
2020-08-24 19:44:23 +0300

    add validation for article body length

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,6 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
-  validates :body, presence: true, length: { minimum: 5, maximum: 300 }
+  validates :body, presence: true
+  validates_length_of :body, minimum: 5, maximum: 300
 end
