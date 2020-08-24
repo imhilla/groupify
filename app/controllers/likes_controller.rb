68fc442706663d64fffaca897b26e5cdb67eaf3b
imhilla <hillaryodhiambo282@gmail.com>
2020-08-24 15:57:19 +0300

    add like functionality to profile page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,5 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(article_id: params[:article_id])
     @like.save
-
-    redirect_to articles_path
   end
 end
