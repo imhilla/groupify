3ef58e89a8864ba8f7e0e3615dbb106cc900ded0
imhilla <hillaryodhiambo282@gmail.com>
2020-08-25 18:24:41 +0300

    fix rubocop errors

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -82,11 +82,13 @@ module ArticlesHelper
       b.to_s + ', ' + c.to_s + ' and ' + who_others_count(article).to_s + ' others liked'
     end
   end
+  # rubocop:disable Style/GuardClause
 
   def faheart(article)
     if article.likes.count.positive?
-      content_tag(:i, :class => "fa fa-heart") do
+      content_tag(:i, class: 'fa fa-heart') do
       end
     end
   end
 end
+# rubocop:enable Style/GuardClause
