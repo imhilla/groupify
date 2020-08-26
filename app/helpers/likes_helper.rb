539e61b2d87c90a500f06bbd3fff30a2579bcf0f
imhilla <hillaryodhiambo282@gmail.com>
2020-08-26 07:10:13 +0300

    remove the check likes helper method

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,9 +1,4 @@
 module LikesHelper
-  def check_likes(article)
-    if article.likes.size.positive?
-    end
-  end
-
   def like_btn(article, page)
     @like = Like.find_by(article: article, user: current_user)
     button_to('', article_likes_path(article_id: article.id, page: page), method: :post, class: 'like-button-dynamic')
