a90f4208c7f762f1ac124b8356a5cf7cd963c13c
imhilla <hillaryodhiambo282@gmail.com>
2020-08-26 12:39:24 +0300

    fix rspec failing tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Comment, type: :model do
   before(:all) do
     User.create(name: 'james', username: 'james', email: 'james@gmail.com', password: '123456')
     Article.create(title: 'How to use ruby on rails', body: 'Rails is a ruby framework that..', author_id: 1)
-    Comment.create(author_name: 'james', body: 'Which resources can i use', article_id: 1, user_name: 'james', user_id: 1)
+    Comment.create(body: 'Which resources can i use', article_id: 1, user_id: 1)
   end
 
   it 'creates a comment in an article' do
